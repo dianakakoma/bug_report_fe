3a61b0393da9211dd7aaeb8ccf99574e21734b7c
dianakakoma <dikakoma@gmail.com>
2020-08-05 19:57:17 -0500

    created and successfully tested the create user button. The index functionality was completed this morning.

---
+++ src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <img src="@/assets/happy-ladybug.png" width="100" />
+    <h1>{{ message }}</h1>
+    <h2>A bug reporting tool built for the end user!</h2>
+    <!-- Sign Up Button -->
+    <button><a href="/signup">Sign up to report bugs or to submit your great idea!</a></button>
+    <hr />
+    <div v-for="report in reports">
+      <p>Report ID:{{ report.id }}</p>
+      <h2>Bug or Bright Idea: {{ report.report_type }}</h2>
+      <h3>Report Description: {{ report.description }}</h3>
+      <p>Suggest Fix: {{ report.suggested_fix }}</p>
+      <p>Current Status:{{ report.status }}</p>
+      <a v-bind:href="report.url">{{ report.url }}</a>
+      <p>User Id: {{ report.user_id }}</p>
+      <p style="color:red">|</p>
+    </div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      message: "Bug Report!",
+      reports: []
+    };
+  },
+  created: function() {
+    axios.get("api/reports").then((response) => {
+      this.reports = response.data;
+    });
+  },
+  methods: {}
+};
+</script>
